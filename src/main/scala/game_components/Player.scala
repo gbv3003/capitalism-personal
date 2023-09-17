20681ef7553d21fc4a21cf8a8d89eba5d142a3c4
Garrett <gbv3003@gmail.com>
2023-09-17 17:45:18 -0400

    Added Player Scala doc

--- src/main/scala/game_components/Player.scala
+++ src/main/scala/game_components/Player.scala
@@ -1,6 +1,12 @@
 package game_components
 
-//******* PLAYER *******
+/** A simulated player who plays capitalism
+  *
+  * @constructor
+  *   create a new person with a name.
+  * @param name
+  *   the person's name
+  */
 class Player(val name: String) {
   var hand = new Hand
   var score = 0
@@ -10,5 +16,66 @@ class Player(val name: String) {
     val sb = new StringBuilder()
     sb ++= name + " Hand: " + hand.show + "\n"
     sb.toString.substring(0, sb.toString.length - 2)
+  }
+
+  /** Plays a card in a players hand
+    *
+    * @param card
+    *   the card to be played
+    */
+  def playCard(card: Card): Unit = {
+    println("To be completed")
+  }
+
+  /** Chooses a card to be played in a players hand. This will always be the
+    * lowest card they can play, with a two being played only if they have no
+    * other options.
+    *
+    * @return
+    *   card the card choosen to be played
+    */
+  def chooseCardPlay = {
+    println("To be completed")
+  }
+
+  /** Chooses a card to be traded to another player from a players hand. This
+    * will always be the lowest non-two card in their hand.
+    *
+    * @return
+    *   card the card choosen to be played
+    */
+  def chooseCardGive = {
+    println("To be completed")
+  }
+
+  /** Chooses a card that the player wants from the other player. This will
+    * always be the highest card in the other players hand, with "two" cards
+    * being the highest card.
+    *
+    * @return
+    *   card the card choosen to be played
+    */
+  def chooseCardWant = {
+    println("To be completed")
+  }
+
+  /** Gives a card to another player from a players hand
+    *
+    * @param card
+    *   the card to be given to another player
+    * @param player
+    *   the player that receives the card
+    */
+  def giveCard(player: Player, card: Card) = {
+    println("To be completed")
+  }
+
+  /** Receives a card from another player
+    *
+    * @param card
+    *   the card to be received from another player
+    */
+  def receiveCard(card: Card) = {
+    println("To be completed")
   }
 }
