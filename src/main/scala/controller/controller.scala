be7c2745c0fe7532c9d79f7865be37bfc9eeddb0
Ben Douglas <Benjamin.Douglas@WestPoint.edu>
2023-10-21 22:38:39 -0400

    Updated GUI

--- src/main/scala/controller/controller.scala
+++ src/main/scala/controller/controller.scala
@@ -1,20 +1,79 @@
-package main
+package controller
 
+import model._
 import scala.swing._
-import model.Model
+import view._
 
-class Controller(view: View, model: Model) {
+/**  Controller for an MVC architecture
+  *  @param model Model for the MVC architecture 
+  *  @param view View for MVC architecture
+  */
+class Controller(model: Model, view: View) {
 
-  def hello = Action("Hello") {
-    view.textArea.text = "Show area"
+  /**  Displays everything necessary to observe and understand the progress of the game
+  */
+  def showGameArea: String = {
+    model.menu.showGameArea
   }
 
-  def Show_Game_Area = Action("Show Game Area"){
-    view.textArea.text = "Show area"
+  /**  shows player names in the order they will play
+  */
+  def showPlayerOrder: String = {
+    model.menu.showPlayerOrder
   }
-        
 
+  /**   moves the player who is currently "up" to the end of the playing order, and advances the "next" player to be "up"
+  */
+  def advanceOrder = Action("Advance") {
+    model.menu.advancePlayerOrder
+    view.update_PlayerOrder
+  }
+
+  /**  performs all actions necessary to prepare the game for the first move; can also be used to reset the game simulation
+  */
+  def initialize = Action("Initialize") {
+    model.menu.initialize
+    view.update_PlayerOrder
+    view.update_GameArea
+  }
+
+  /**  at any point in the game simulation, determine whether any player has won the game, and return the winning player's name or "none"
+  */
+  def checkForWinner = Action("Winner?") {    
+    view.showWinner(model.menu.checkForWinner)
+  }
+
+  /**  the player who is currently "up" in the player order performs all move actions, and the player order advances
+  */
+  def doMove = Action("Do Move") {
+    val result = model.menu.doMove
+    view.update_PlayerOrder
+    view.update_GameArea
+    if result then view.showWinner(model.menu.checkForWinner)     
+  }
+  
+  /**  performs DO MOVE four times (or more specfically, the length of the player order), as well as CHECK FOR WINNER at appropriate times
+  */
+  def doTurn = Action("Do Turn") {    
+    val result = model.menu.doTurn
+    view.update_PlayerOrder
+    view.update_GameArea
+    if result then view.showWinner(model.menu.checkForWinner)   
+  }
+  
+  /**  performs DO TURN until the game is won
+  */
+  def doGame = Action("Do Game") {
+    val result = model.menu.doGame
+    view.update_PlayerOrder
+    view.update_GameArea
+    if result then view.showWinner(model.menu.checkForWinner)   
+  }
+
+  /**  terminates the application
+  */
   def exit = Action("Exit") {
     sys.exit(0)
   }
+
 }
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,14 +1,18 @@
-package main
+package _main
 
 import model._
+import view._
+import controller._
 
-object GUI {
+/**  Game simulation for the GridMaster4 game, a turn-based, 4-player game.
+  */
+object GridMaster4 {
   
   @main def main(): Unit = {
       
     val model = new Model
-    val view  = new View   
-    val controller = new Controller(view, model)
+    val view  = new SimpleView   
+    val controller = new Controller(model, view)
     
     view.init(controller)    
   }
